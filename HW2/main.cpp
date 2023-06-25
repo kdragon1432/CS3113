6de15a1b7ed995961a4ab4c02bd6b36040f859a6
Kelly Dong <kellydong1432@gmail.com>
2023-06-25 03:54:16 -0400

    p1 and movemnt

--- HW2/main.cpp
+++ HW2/main.cpp
@@ -59,6 +59,8 @@ glm::vec3 ball_rotation = glm::vec3(0.0f, 0.0f, 0.0f);
 
 ShaderProgram p1;
 glm::mat4 p1_view, p1_matrix, p1_projection;
+const char PLAYER1_SPRITE_FILEPATH[] = "player1.png";
+GLuint p1_texture_id;
 
 glm::vec3 p1_position = glm::vec3(0.0f, 0.0f, 0.0f);
 glm::vec3 p1_movement = glm::vec3(0.0f, 0.0f, 0.0f);
@@ -118,7 +120,7 @@ void initialize() {
 	glViewport(VIEWPORT_X, VIEWPORT_Y, VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
 	// LOAD UP OUR SHADERS
 	BALL.Load(VT_SHADER_PATH, FT_SHADER_PATH);
-	p1.Load(V_SHADER_PATH, F_SHADER_PATH);
+	p1.Load(VT_SHADER_PATH, FT_SHADER_PATH);
 
 	// Initialise our view, model, and projection matrices
 	ball_view = glm::mat4(1.0f);  
@@ -134,7 +136,7 @@ void initialize() {
 	p1.SetViewMatrix(p1_view);
 
 
-	p1.SetColor(1.0f, 0.4f, 0.4f, 1.0f);
+	//p1.SetColor(1.0f, 0.4f, 0.4f, 1.0f);
 
 	// Each object has its own unique ID
 	glUseProgram(BALL.programID);
@@ -144,6 +146,7 @@ void initialize() {
 	glClearColor(BG_RED, BG_BLUE, BG_GREEN, BG_OPACITY); // Background: WHITE
 
 	ball_texture_id = load_texture(BALL_SPRITE_FILEPATH);
+	p1_texture_id = load_texture(PLAYER1_SPRITE_FILEPATH);
 
 	// enable blending
 	glEnable(GL_BLEND);
@@ -167,10 +170,10 @@ void process_input(){
 					ball_movement.y = -1.0f;
 					break;
 				case SDLK_w:
-					ball_movement.y = 1.0f;
+					p1_movement.y = 1.0f;
 					break;
 				case SDLK_s:
-					ball_movement.y = -1.0f;
+					p1_movement.y = -1.0f;
 					break;
 				default:
 					break;
@@ -184,10 +187,10 @@ void process_input(){
 					ball_movement.y = 0.0f;
 					break;
 				case SDLK_w:
-					ball_movement.y = 0.0f;
+					p1_movement.y = 0.0f;
 					break;
 				case SDLK_s:
-					ball_movement.y = 0.0f;
+					p1_movement.y = 0.0f;
 					break;
 				default:
 					break;
@@ -206,15 +209,18 @@ void process_input(){
 	}
 	if (key_state[SDL_SCANCODE_W])
 	{
-		ball_movement.y = 1.0f;
+		p1_movement.y = 1.0f;
 	}
 	else if (key_state[SDL_SCANCODE_S]){
-		ball_movement.y = -1.0f;
+		p1_movement.y = -1.0f;
 	}
 
 	// This makes sure that the player can't "cheat" their way into moving faster
 	if (glm::length(ball_movement) > 1.0f){
 		ball_movement = glm::normalize(ball_movement);
+	}
+	if (glm::length(p1_movement) > 1.0f) {
+		p1_movement = glm::normalize(p1_movement);
 	}
 }
 
@@ -231,9 +237,12 @@ void update() {
 
 	// Add direction * units per second * elapsed time
 	ball_position += ball_movement * player_speed * delta_time;
+	p1_position += p1_movement * player_speed * delta_time;
 
 	ball_matrix = glm::mat4(1.0f);
 	ball_matrix = glm::translate(ball_matrix, ball_position);
+	p1_matrix = glm::mat4(1.0f);
+	p1_matrix = glm::translate(p1_matrix, p1_position);
 
 	//model_matrix = glm::rotate(model_matrix, (ROT_ANGLE * delta_time), glm::vec3(0.0f, 0.0f, 1.0f));
 
@@ -277,10 +286,21 @@ void render() {
 		-4.5f, 1.0f,
 	};
 
+	float texture_coordinates1[] = {
+		0.0f, 0.5f, 
+		0.4f, 0.0f, 
+		0.4f, 0.1f, 
+
+	};
+
 	glVertexAttribPointer(p1.positionAttribute, 2, GL_FLOAT, false, 0, vertices1);
 	glEnableVertexAttribArray(p1.positionAttribute);
+	glVertexAttribPointer(p1.texCoordAttribute, 2, GL_FLOAT, false, 0, texture_coordinates1);
+	glEnableVertexAttribArray(p1.texCoordAttribute);
+	glBindTexture(GL_TEXTURE_2D, p1_texture_id);
 	glDrawArrays(GL_TRIANGLES, 0, 3);
 	glDisableVertexAttribArray(p1.positionAttribute);
+	glDisableVertexAttribArray(p1.texCoordAttribute);
 
 	SDL_GL_SwapWindow(displayWindow);
 
@@ -303,218 +323,3 @@ int main(int argc, char* argv[]) {
 	shutdown();
 	return 0;
 }
-
-/**
-* Author: Kelly Dong
-* Assignment: Pong Clone
-* Date due: 2023-06-23, 11:59pm
-* I pledge that I have completed this assignment without
-* collaborating with anyone else, in conformance with the
-* NYU School of Engineering Policies and Procedures on
-* Academic Misconduct.
-
-
-#define GL_SILENCE_DEPRECATION
-#define GL_GLEXT_PROTOTYPES 1
-#define STB_IMAGE_IMPLEMENTATION
-#define LOG(argument) std::cout << argument << '\n'
-
-#ifdef _WINDOWS
-#include <GL/glew.h>
-#endif
-
-#include <SDL.h>
-#include <SDL_opengl.h>
-#include "glm/mat4x4.hpp"                // 4x4 Matrix
-#include "glm/gtc/matrix_transform.hpp"  // Matrix transformation methods
-#include "ShaderProgram.h"  
-#include "stb_image.h"
-
-
-const int	WINDOW_WIDTH  = 640,
-			WINDOW_HEIGHT = 480;
-
-// Background Color: WHITE
-const float		BG_RED		= 1.0f, // LIGHT BLUE 
-				BG_BLUE		= 1.0f, // PINK
-				BG_GREEN	= 1.0f, // YELLOW
-				BG_OPACITY  = 1.0f;
-
-// Our viewport—or our "camera"'s—position and dimensions
-const int		VIEWPORT_X = 0,
-				VIEWPORT_Y = 0,
-				VIEWPORT_WIDTH  = WINDOW_WIDTH,
-				VIEWPORT_HEIGHT = WINDOW_HEIGHT;
-
-
-
-// SOLID COLOR SHADERS
-const char		V_SHADER_PATH[] = "shaders/vertex.glsl",
-				F_SHADER_PATH[] = "shaders/fragment.glsl";
-// TEXTURED SHADERS
-const char		VT_SHADER_PATH[] = "shaders/vertex_textured.glsl",
-				FT_SHADER_PATH[] = "shaders/fragment_textured.glsl";
-
-
-
-		
-
-
-SDL_Window* displayWindow;
-bool gameIsRunning = true;
-
-
-ShaderProgram program;
-	glm::mat4	view_matrix,        // Defines the position (location and orientation) of the camera
-				model_matrix,       // Defines every translation, rotation, and/or scaling applied to an object; we'll look at these next week
-				projection_matrix;  // Defines the characteristics of your camera, such as clip panes, field of view, projection method, etc.
-const char BALL_SPRITE[] = "ball.png";
-GLuint ball_texture_id;
-
-
-const int NUMBER_OF_TEXTURES = 1; // to be generated, that is
-const GLint LEVEL_OF_DETAIL = 0; // base image level; Level n is the nth mipmap reduction image
-const GLint TEXTURE_BORDER = 0; // this value MUST be zero
-
-GLuint load_texture(const char* filepath)
-{
-	// STEP 1: Loading the image file
-	int width, height, number_of_components;
-	unsigned char* image = stbi_load(filepath, &width, &height, &number_of_components, STBI_rgb_alpha);
-
-	if (image == NULL)
-	{
-		LOG("Unable to load image. Make sure the path is correct.");
-		assert(false);
-	}
-
-	// STEP 2: Generating and binding a texture ID to our image
-	GLuint textureID;
-	glGenTextures(NUMBER_OF_TEXTURES, &textureID);
-	glBindTexture(GL_TEXTURE_2D, textureID);
-	glTexImage2D(GL_TEXTURE_2D, LEVEL_OF_DETAIL, GL_RGBA, width, height, TEXTURE_BORDER, GL_RGBA, GL_UNSIGNED_BYTE, image);
-
-	// STEP 3: Setting our texture filter parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
-	// STEP 4: Releasing our file from memory and returning our texture id
-	stbi_image_free(image);
-
-	return textureID;
-}
-
-
-void initialize() {
-	SDL_Init(SDL_INIT_VIDEO);
-	displayWindow = SDL_CreateWindow("HW2 PONG CLONE", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_OPENGL);
-	SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
-	SDL_GL_MakeCurrent(displayWindow, context);
-
-#ifdef _WINDOWS
-	glewInit();
-#endif
-	// INITIALIZE OUR CAMERA
-	glViewport(VIEWPORT_X, VIEWPORT_Y, VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
-
-	program.Load(VT_SHADER_PATH, FT_SHADER_PATH);
-
-	model_matrix = glm::mat4(1.0f);
-	view_matrix = glm::mat4(1.0f);  // Defines the position (location and orientation) of the camera
-	projection_matrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);  // Defines the characteristics of your camera, such as clip planes, field of view, projection method etc.
-
-	program.SetProjectionMatrix(projection_matrix);
-	program.SetViewMatrix(view_matrix);
-	// Notice we haven't set our model matrix yet!
-
-	glUseProgram(program.programID);
-
-	glClearColor(BG_RED, BG_BLUE, BG_GREEN, BG_OPACITY);
-
-	ball_texture_id = load_texture(BALL_SPRITE);
-
-	// enable blending
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-}
-
-void process_input() {
-	SDL_Event event;
-	while (SDL_PollEvent(&event)){
-		switch (event.type){
-			case SDL_QUIT:
-			case SDL_WINDOWEVENT_CLOSE:
-				gameIsRunning = false;
-				break;
-			default:
-				break;
-		}
-	}
-}
-
-
-int frame_counter = 0;
-float previous_ticks = 0;
-const float ROT_ANGLE = glm::radians(90.0f);
-void update() {
-	float ticks = (float)SDL_GetTicks() / 1000.0f;  // get the current number of ticks
-	float delta_time = ticks - previous_ticks;
-	previous_ticks = ticks;
-	glm::vec3 scale_vector;
-	//frame_counter += 1;
-
-	model_matrix = glm::rotate(model_matrix, (ROT_ANGLE*delta_time), glm::vec3(0.0f, 0.0f, 1.0f));
-
-}
-
-void render() {
-	glClear(GL_COLOR_BUFFER_BIT);
-
-	// Vertices
-	float vertices[] = {
-		-0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f,  // triangle 1
-		-0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f   // triangle 2
-	};
-
-	glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
-	glEnableVertexAttribArray(program.positionAttribute);
-
-	// Textures
-	float texture_coordinates[] = {
-		0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,     // triangle 1
-		0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f,     // triangle 2
-	};
-
-	glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texture_coordinates);
-	glEnableVertexAttribArray(program.texCoordAttribute);
-
-	// Bind texture
-	glBindTexture(GL_TEXTURE_2D, ball_texture_id);
-	glDrawArrays(GL_TRIANGLES, 0, 6); // we are now drawing 2 triangles, so we use 6 instead of 3
-
-	// We disable two attribute arrays now
-	glDisableVertexAttribArray(program.positionAttribute);
-	glDisableVertexAttribArray(program.texCoordAttribute);
-
-	SDL_GL_SwapWindow(displayWindow);
-
-}
-
-void shutdown() {
-	SDL_Quit();
-}
-
-int main(int argc, char* argv[]) {
-	initialize();
-
-	while (gameIsRunning) {
-		process_input();
-		update();
-		render();
-	}
-
-	shutdown();
-	return 0;
-}
-**/
