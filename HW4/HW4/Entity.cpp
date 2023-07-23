63e810ca7d3a4e32f71d74182021445ef91c6c76
Kelly Dong <kellydong1432@gmail.com>
2023-07-23 01:25:19 -0400

    project 4 edit

--- HW4/HW4/Entity.cpp
+++ HW4/HW4/Entity.cpp
@@ -15,7 +15,11 @@
 
 Entity::Entity()
 {
-    m_position = glm::vec3(0);
+    m_movement = glm::vec3(0.0f);
+    m_position = glm::vec3(0.0f);
+    m_velocity = glm::vec3(0.0f);
+    m_acceleration = glm::vec3(0.0f);
+
     m_speed = 0;
     m_model_matrix = glm::mat4(1.0f);
 }
@@ -31,15 +35,12 @@ Entity::~Entity()
 
 void Entity::draw_sprite_from_texture_atlas(ShaderProgram* program, GLuint texture_id, int index)
 {
-    // Step 1: Calculate the UV location of the indexed frame
     float u_coord = (float)(index % m_animation_cols) / (float)m_animation_cols;
     float v_coord = (float)(index / m_animation_cols) / (float)m_animation_rows;
 
-    // Step 2: Calculate its UV size
     float width = 1.0f / (float)m_animation_cols;
     float height = 1.0f / (float)m_animation_rows;
 
-    // Step 3: Just as we have done before, match the texture coordinates to the vertices
     float tex_coords[] =
     {
         u_coord, v_coord + height, u_coord + width, v_coord + height, u_coord + width, v_coord,
@@ -52,7 +53,6 @@ void Entity::draw_sprite_from_texture_atlas(ShaderProgram* program, GLuint textu
         -0.5, -0.5, 0.5,  0.5, -0.5, 0.5
     };
 
-    // Step 4: And render
     glBindTexture(GL_TEXTURE_2D, texture_id);
 
     glVertexAttribPointer(program->positionAttribute, 2, GL_FLOAT, false, 0, vertices);
@@ -67,8 +67,63 @@ void Entity::draw_sprite_from_texture_atlas(ShaderProgram* program, GLuint textu
     glDisableVertexAttribArray(program->texCoordAttribute);
 }
 
-void Entity::update(float delta_time)
+void Entity::ai_activate(Entity* player)
 {
+    switch (m_ai_type)
+    {
+    case WALKER:
+        ai_walk();
+        break;
+
+    case GUARD:
+        ai_guard(player);
+        break;
+
+    default:
+        break;
+    }
+}
+
+void Entity::ai_walk()
+{
+    m_movement = glm::vec3(-1.0f, 0.0f, 0.0f);
+}
+
+void Entity::ai_guard(Entity* player)
+{
+    switch (m_ai_state) {
+    case IDLE:
+        if (glm::distance(m_position, player->get_position()) < 3.0f) m_ai_state = WALKING;
+        break;
+
+    case WALKING:
+        if (m_position.x > player->get_position().x) {
+            m_movement = glm::vec3(-1.0f, 0.0f, 0.0f);
+        }
+        else {
+            m_movement = glm::vec3(1.0f, 0.0f, 0.0f);
+        }
+        break;
+
+    case ATTACKING:
+        break;
+
+    default:
+        break;
+    }
+}
+
+void Entity::update(float delta_time, Entity* player, Entity* collidable_entities, int collidable_entity_count)
+{
+    if (!m_is_active) return;
+
+    m_collided_top = false;
+    m_collided_bottom = false;
+    m_collided_left = false;
+    m_collided_right = false;
+
+    if (m_entity_type == ENEMY) ai_activate(player);
+
     if (m_animation_indices != NULL)
     {
         if (glm::length(m_movement) != 0)
@@ -89,13 +144,77 @@ void Entity::update(float delta_time)
         }
     }
 
-    m_position += m_movement * m_speed * delta_time;
+    m_velocity.x = m_movement.x * m_speed;
+    m_velocity += m_acceleration * delta_time;
+
+    m_position.y += m_velocity.y * delta_time;
+    check_collision_y(collidable_entities, collidable_entity_count);
+
+    m_position.x += m_velocity.x * delta_time;
+    check_collision_x(collidable_entities, collidable_entity_count);
+
+    if (m_is_jumping)
+    {
+        m_is_jumping = false;
+        m_velocity.y += m_jumping_power;
+    }
+
     m_model_matrix = glm::mat4(1.0f);
     m_model_matrix = glm::translate(m_model_matrix, m_position);
 }
 
+void const Entity::check_collision_y(Entity* collidable_entities, int collidable_entity_count)
+{
+    for (int i = 0; i < collidable_entity_count; i++)
+    {
+        Entity* collidable_entity = &collidable_entities[i];
+
+        if (check_collision(collidable_entity))
+        {
+            float y_distance = fabs(m_position.y - collidable_entity->get_position().y);
+            float y_overlap = fabs(y_distance - (m_height / 2.0f) - (collidable_entity->get_height() / 2.0f));
+            if (m_velocity.y > 0) {
+                m_position.y -= y_overlap;
+                m_velocity.y = 0;
+                m_collided_top = true;
+            }
+            else if (m_velocity.y < 0) {
+                m_position.y += y_overlap;
+                m_velocity.y = 0;
+                m_collided_bottom = true;
+            }
+        }
+    }
+}
+
+void const Entity::check_collision_x(Entity* collidable_entities, int collidable_entity_count)
+{
+    for (int i = 0; i < collidable_entity_count; i++)
+    {
+        Entity* collidable_entity = &collidable_entities[i];
+
+        if (check_collision(collidable_entity))
+        {
+            float x_distance = fabs(m_position.x - collidable_entity->get_position().x);
+            float x_overlap = fabs(x_distance - (m_width / 2.0f) - (collidable_entity->get_width() / 2.0f));
+            if (m_velocity.x > 0) {
+                m_position.x -= x_overlap;
+                m_velocity.x = 0;
+                m_collided_right = true;
+            }
+            else if (m_velocity.x < 0) {
+                m_position.x += x_overlap;
+                m_velocity.x = 0;
+                m_collided_left = true;
+            }
+        }
+    }
+}
+
 void Entity::render(ShaderProgram* program)
 {
+    if (!m_is_active) return;
+
     program->SetModelMatrix(m_model_matrix);
 
     if (m_animation_indices != NULL)
@@ -118,4 +237,16 @@ void Entity::render(ShaderProgram* program)
 
     glDisableVertexAttribArray(program->positionAttribute);
     glDisableVertexAttribArray(program->texCoordAttribute);
+}
+
+bool const Entity::check_collision(Entity* other) const
+{
+
+    if (other == this) return false; // If we are checking with collisions with ourselves, this should be false
+    if (!m_is_active || !other->m_is_active) return false;
+
+    float x_distance = fabs(m_position.x - other->get_position().x) - ((m_width + other->get_width()) / 2.0f);
+    float y_distance = fabs(m_position.y - other->get_position().y) - ((m_height + other->get_height()) / 2.0f);
+
+    return x_distance < 0.0f && y_distance < 0.0f;
 }
