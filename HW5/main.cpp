2034e8f8067c21a159e233b0a98b3a4c723db5a0
Kelly Dong <kellydong1432@gmail.com>
2023-08-02 01:42:37 -0400

    hi

--- HW5/main.cpp
+++ HW5/main.cpp
@@ -7,12 +7,17 @@
 #include <SDL.h>
 #include <SDL_opengl.h>
 
+const int WINDOW_WIDTH=640, WINDOW_HEIGHT=480;
+const float BG_RED=1.0f, BG_GREEN=1.0f, BG_BLUE=1.0f, BG_OPACITY=1.0f;
+
 SDL_Window* displayWindow;
 bool gameIsRunning = true;
 
-int main(int argc, char* argv[]) {
+
+void initialise()
+{
 	SDL_Init(SDL_INIT_VIDEO);
-	displayWindow = SDL_CreateWindow("Hello, World!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
+	displayWindow = SDL_CreateWindow("HW5!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_OPENGL);
 	SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
 	SDL_GL_MakeCurrent(displayWindow, context);
 
@@ -20,20 +25,37 @@ int main(int argc, char* argv[]) {
 	glewInit();
 #endif
 
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClearColor(BG_RED, BG_GREEN, BG_BLUE, BG_OPACITY);
+}
+void process_input()
+{
+	SDL_Event event;
+	while (SDL_PollEvent(&event)) {
+		if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
+			gameIsRunning = false;
+		}
+	}
+
+}
+void update() {}
+void render() 
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	SDL_GL_SwapWindow(displayWindow);
+}
+
+void shutdown() { SDL_Quit(); }
+
+int main(int argc, char* argv[]) {
+	initialise();
 
 	SDL_Event event;
 	while (gameIsRunning) {
-		while (SDL_PollEvent(&event)) {
-			if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
-				gameIsRunning = false;
-			}
-		}
-
-		glClear(GL_COLOR_BUFFER_BIT);
-		SDL_GL_SwapWindow(displayWindow);
+		process_input();
+		update();
+		render();
 	}
 
-	SDL_Quit();
+	shutdown();
 	return 0;
 }
